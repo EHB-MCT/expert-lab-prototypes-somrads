1f11d581bf1a758703d2171b2b04a52bbfdedccf
somrad <somrad.sharma@student.ehb.be>
2022-11-06 14:04:32 +0100

    Added socket join room

--- chat-app/server/server.js
+++ chat-app/server/server.js
@@ -1,8 +1,7 @@
-import express from "express";
-const app = express();
+const app = require("express")();
 const server = require("http").createServer(app);
 const PORT = 5000;
-import { nanoid } from "nanoid";
+const {nanoid} = require("nanoid");
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
@@ -12,29 +11,55 @@ const io = require("socket.io")(server, {
 const users = [];
 const rooms = [];
 
-// when client connect to server will get back socket.id
+// User connect
 io.on("connection", (socket) => {
   socket.emit("me", socket.id);
   users.push(socket.id);
 
-  // sending users array to all clients with event
+  // Updating users array
   socket.broadcast.emit("updateUsers", users);
 
+  // User disconnect
   socket.on("disconnect", () => {
     users = users.filter((user) => user !== socket.id);
     socket.broadcast.emit("updateUsers", users);
     socket.disconnect();
   });
 
+  // Fetch users array
   socket.emit("getAllUsers", users);
 
-  // Rooms
+  // Create Rooms
   socket.on("createRoom", () => {
     const room = {
       id: nanoid(5),
       chat: [],
     };
+    socket.join(room);
+    socket.emit("getRoom", room);
+    rooms.push(room);
+    socket.broadcast.emit("updateRooms", rooms);
   });
 
-  
+  //Join Rooms
+  socket.on("joinRoom", (room) => {
+    socket.join(room.id);
+  });
+  socket.broadcast.emit("updateRooms", rooms);
+
+  // Message
+  socket.on("message", (payLoad) => {
+    rooms.map((room) => {
+      if (room.id === payLoad.room) {
+        singleChat = { message: payLoad.message, writer: payLoad.socketId };
+        room.chat.push(singleChat);
+      }
+    });
+    // sends message to given room
+    io.to(payLoad.room).emit("chat", payLoad);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
 });
