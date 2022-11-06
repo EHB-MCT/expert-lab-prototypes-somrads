f3457fa927b6f9bcc458a440d40a13d2fe4f01c5
somrad <somrad.sharma@student.ehb.be>
2022-11-06 12:55:36 +0100

    created express server with socket
    and created users and rooms array and user filtering based on id

---
+++ chat-app/server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+const app = express();
+const server = require("http").createServer(app);
+const PORT = 5000;
+import { nanoid } from "nanoid";
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const users = [];
+const rooms = [];
+
+// when client connect to server will get back socket.id
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+  users.push(socket.id);
+
+  // sending users array to all clients with event
+  socket.broadcast.emit("updateUsers", users);
+
+  socket.on("disconnect", () => {
+    users = users.filter((user) => user !== socket.id);
+  });
+});
