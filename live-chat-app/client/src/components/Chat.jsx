2b38a7b910759b68fd299e897b7f706f6bcd0ba1
somrad <somrad.sharma@student.ehb.be>
2022-11-10 22:56:37 +0100

    Added chat css

--- live-chat-app/client/src/components/Chat.jsx
+++ live-chat-app/client/src/components/Chat.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/chat.scss";
 
 function Chat() {
   return (
-    <div>Chat</div>
-  )
+    <div className="container">
+      <div className="leftside">
+        <h1>spacechat</h1>
+        <h2>Online</h2>
+        <ul className="onlineUsers">
+          <li>Tom</li>
+          <li>John</li>
+          <li>Jacob</li>
+          <li>Jesse</li>
+        </ul>
+        <button>Leave</button>
+      </div>
+      <div className="rightside">
+        <div className="chatdisplay">
+          <div className="messages">
+            <h3 className="messageLeft">Text</h3>
+            <h3 className="messageRight">Text</h3>
+
+          </div>
+        </div>
+        <div className="titleBar">
+          <h2>Apollo 13</h2>{" "}
+        </div>
+        <div className="messages"></div>
+        <div className="chatBar">
+          <div className="chatInput">
+            <input type="text" />
+            <button>Send</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
