991dd37e2d32cd62c8cc583060ee9db2ce94cbdf
somrad <somrad.sharma@student.ehb.be>
2022-11-29 10:10:01 +0100

    html text added

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,24 +1,47 @@
 import React from "react";
 import "../styles/hero.scss";
+import Logo from "../assets/logo.svg";
+import Icon from "../assets/menu-mobile.svg";
 
 function Hero() {
   return (
     <div className="Hero">
-      <div className="nav">
-        <h5>somrad</h5>
-        <h5>navblock</h5>
-      </div>
-      <div className="hero-section">
-        <h2>Creative Developer</h2>
-        <h2>Somrad Sharma</h2>
+      <header class="header">
+        {/* <!-- Logo --> */}
+        <a href="/">
+          <img src={Logo} alt="logo" class="logo" />
+        </a>
+        {/* <!-- Hamburger icon --> */}
+        <input class="side-menu" type="checkbox" id="side-menu" />
+        <label class="hamb" for="side-menu">
+          <span class="hamb-line"></span>
+        </label>
+        {/* <!-- Menu --> */}
+        <nav class="nav">
+          <ul class="menu">
+            <li>
+              <a href="#">1.About</a>
+            </li>
+            <li>
+              <a href="#">2.Projects</a>{" "}
+            </li>
+            <li>
+              <a href="#">3.Contact</a>
+            </li>
+          </ul>
+          <button>Resume</button>
+        </nav>
+      </header>
+
+      {/* <div className="hero-section">
+        <h1>Creative Developer</h1>
+        <h1>Somrad Sharma</h1>
         <p>
           I make cool stuff on the web. I am currenty looking for a internship
           as a designer/ front end developer.
         </p>
-        <button>
-          Resume
-        </button>
-      </div>
+        <button>Resume</button>
+      </div> */}
     </div>
   );
 }
--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -4,12 +4,13 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import './styles/main.scss'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Hero />
-    <About />
+    {/* <About />
     <Projects />
-    <Contact />
+    <Contact /> */}
   </React.StrictMode>
 );
