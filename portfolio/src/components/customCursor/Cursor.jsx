e8c56ccb268191ab3efcdf60be55ada4056284ea
somrad <somrad.sharma@student.ehb.be>
2022-12-15 14:29:00 +0100

    completed custom cursor, hover and click

--- portfolio/src/components/customCursor/Cursor.jsx
+++ portfolio/src/components/customCursor/Cursor.jsx
@@ -1,37 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import './cursor.scss'
+import React, { useState, useEffect } from "react";
+import "./cursor.scss";
 
 const Cursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [hidden, setHidden] = useState(false);
+  const [click, setClick] = useState(false);
+  const [linkHover, setLinkHover] = useState(false);
 
   useEffect(() => {
     const addEventListeners = () => {
-      document.addEventListener('mousemove',mMove);
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseenter", handleMouseEnter);
+      document.addEventListener("mouseleave", handleMouseLeave);
+      document.addEventListener("mousedown", handleMouseDown);
+      document.addEventListener("mouseup", handleMouseUp);
     };
 
     const removeEventListeners = () => {
-      document.removeEventListener('mousemove', mMove);
-
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseenter", handleMouseEnter);
+      document.removeEventListener("mouseleave", handleMouseLeave);
+      document.removeEventListener("mousedown", handleMouseDown);
+      document.removeEventListener("mouseup", handleMouseUp);
     };
 
-    const mMove = (el) => {
-      setPosition({ x: el.clientX, y: el.clientY });
+    const handleMouseDown = () => {
+      setClick(true);
     };
-  
+
+    const handleMouseUp = () => {
+      setClick(false);
+    };
+
+    const handleMouseMove = (event) => {
+      setPosition({ x: event.clientX, y: event.clientY });
+    };
+
+    const handleMouseLeave = () => {
+      setHidden(true);
+    };
+
+    const handleMouseEnter = () => {
+      setHidden(false);
+    };
+
+    const addLinkEvents = () => {
+      document.querySelectorAll("a").forEach((el) => {
+        el.addEventListener("mouseover", () => setLinkHover(true));
+        el.addEventListener("mouseout", () => setLinkHover(false));
+      });
+      document.querySelectorAll("button").forEach((el) => {
+        el.addEventListener("mouseover", () => setLinkHover(true));
+        el.addEventListener("mouseout", () => setLinkHover(false));
+      });
+    };
+
     addEventListeners();
+    addLinkEvents();
+
     return () => removeEventListeners();
   }, []);
 
+  const cursorClass = [
+    "cursor",
+    click ? "cursor--click" : "",
+    hidden ? "cursor--hidden" : "",
+    linkHover ? "cursor--hover" : "",
+  ].join(" ");
 
-  return (
-    <div
-      className={'cursor '}
-      style={{
-        left: `${position.x}px`,
-        top: `${position.y}px`,
-      }}
-    />
-  );
+  const cursorStyles = {
+    left: `${position.x}px`,
+    top: `${position.y}px`,
+  };
+
+  return <div className={cursorClass} style={cursorStyles} />;
 };
 
 export default Cursor;
